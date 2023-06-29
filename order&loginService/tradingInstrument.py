2065bd55d583b0ffdfdb8ce2c8fc0c6299a9d3d2
Siddharth Lahoty <siddharthlahoty@Siddharths-MacBook-Pro.local>
2023-06-29 10:26:09 +0530

    cleanup

--- order&loginService/tradingInstrument.py
+++ order&loginService/tradingInstrument.py
@@ -46,49 +46,4 @@ insertData()
 
 
 
-
-# def insertData(data,exchange):
-#     print(len(data),exchange,data[0])
-#     if exchange == 'NSE':
-#         colection = db['instrumentNSE']
-#         for item in data:
-#             print(item,"inside forrrrr")
-#             item['expiry'] = item['expiry'].strftime('%Y-%m-%d')
-#     if exchange == 'NFO':
-#         print("inside ifffff")
-#         colection = db['instrumentNFO']
-#         for item in data:
-#             print(item,"inside forrrrr")
-#             item['expiry'] = item['expiry'].strftime('%Y-%m-%d')
-#             print(item,"data inside iffff")
-#     try:
-#         # print(data[0])
-#         colection.insert_many(data)
-#     except Exception as e:
-#         print("Insert failed: {}".format(e))
-
-# dataNFO = download_instruments('NFO')
-
-
-# insertData(dataNFO,'NSE')
-
-# colectionName = db['instrumentNFO']
-# try:
-#     colectionName.insert_many(dataNFO)
-# except Exception as e:
-#         print("Insert failed: {}".format(e))
-
-# dataNSE = download_instruments('NSE')
-
-# count = 0
-
-     
-
-# collectionName = db['instrumentNSE']
-# try:
-#     collectionName.insert_many(dataNSE)
-# except Exception as e:
-#     print("Insert failer{}".format(e))
-
-
      
